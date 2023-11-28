8b573b2942bf1aabf5881d8566447f9481ab0ddb
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-28 15:10:38 +0100

    Feature: Creazione Divs

--- script/myScript.js
+++ script/myScript.js
@@ -3,3 +3,20 @@
 // 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+const mainContainer = document.querySelector('.container');
+
+const playButton = document.getElementById('play');
+
+playButton.addEventListener('click', function() {
+
+  for (let i = 1; i <= 100; i++) {
+
+    const createSquare = document.createElement('div');
+
+    createSquare.classList.add('square');
+
+    mainContainer.append(createSquare);
+
+  }
+
+})
