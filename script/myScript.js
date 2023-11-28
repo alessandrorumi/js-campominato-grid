e7df8d852ea1093773bc854c9f47ca8bf5476fa4
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-28 14:31:33 +0100

    Start: Creazione file

---
+++ script/myScript.js
@@ -0,0 +1,4 @@
+// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// 10 caselle per ognuna delle 10 righe.
+// Quando l’utente clicca su ogni cella, si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
